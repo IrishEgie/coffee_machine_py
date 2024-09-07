53ccd524d089b3c83149730b36c79901b3a6c65a
ejarao <irishegie.arao@ssu.edu.ph>
2024-09-07 12:56:51 +0800

    added transaction function to coffee machine

--- coffee_machine.py
+++ coffee_machine.py
@@ -12,15 +12,58 @@ def print_report():
     for key in res:
         print(f"Current Coffee Machine {key}: {res[key]}")
 
+def transaction(coffee_name, coffee_cost):
+    print(f"Great Choice! The {coffee_name} is: ${coffee_cost}")
+    print("-"*110)
 
+    payment = []
+    print("Please input payment in the amounts of, [1] Penny ($0.01) [2] Nickel($0.05) [3] Dimes ($ 0.10) [4] Quarters ($0.25): ")
+    transc = {"penny":0.01,
+                "nickel":0.05,
+                "dime":0.10,
+                "quarter":0.25}
+
+    for key in transc:
+        try:
+            paid_coins = int(input(f"{key.capitalize()}(Number of {key.capitalize()}/s): "))
+        except ValueError:
+            print("An error during transaction occured, Refunding & closing transaction ... ")
+            print(f"Total coins refunded: {sum(payment)}")
+            return False
+
+        payment.append(transc[key] * paid_coins)
+    print("="*110)
+    print("Processing Transaction...")
+    
+
+    change = sum(payment) - coffee_cost
+
+    if change < 0:
+        print("="*110)
+        print("Transaction Failed...")
+        print("-"*110)
+        print("Sorry that's not enough money. Money refunded.")
+        print(f"Total coins refunded: {sum(payment)}")
+        print("="*110)
+        return False
+
+    else: 
+        print("="*110)
+        print("Transaction successful ...")
+        print("-"*110)
+        print(f"Total coins recieved: ${sum(payment)}")
+        print(f"- {coffee_name}({coffee_cost})\nChange: {change}")
+        return True
+    
 print(logo)
 while power:
+    
     #initialize blank resource dictionary
     res_cache = {}
 
-
-    print("Note: type 'report' to generate report of the current coffee machine resources, 'off' to turn off the program")
     print("="*110)
+    print("Note: type 'report' to generate report of the current coffee machine resources, 'off' to turn off the program")
+    print("-"*110)
 
 
     user_input = input("What would you like? (espresso/latte/cappuccino): ").lower()
@@ -33,34 +76,35 @@ while power:
         power = True
     elif user_input == "espresso" or user_input == "latte" or user_input == "cappuccino":
         
-        
-        #selected menu values
+        #selected menu values 
         u_water = menu[user_input]["ingredients"]["water"]
         u_milk = menu[user_input]["ingredients"]["milk"]
         u_coffee = menu[user_input]["ingredients"]["coffee"]
         u_cost = menu[user_input]["cost"]
-        print(f"This is the selected menu's cost is: ${u_cost}")
-        print("-"*110)
-        payment = int(input("Please input payment [1] Penny ($0.01) [2] Nickel($0.05) [3] Dimes ($ 0.10) [4] Quarters ($0.25): "))
 
-    
-        #res_k == Coffee Machine resources
-        res_w= res["water"]
-        res_m= res["milk"]
-        res_c= res["coffee"]
+        coffee_lease = transaction(user_input.capitalize(), u_cost )
 
-        res_cache["water"] = res_w - u_water
-        res_cache["milk"] = res_m - u_milk
-        res_cache["coffee"] = res_c - u_coffee
         
-        #the cache's value is now the res value
-        res = res_cache
+        if coffee_lease == True:
+        #res_k == Coffee Machine resources
+            res_w= res["water"]
+            res_m= res["milk"]
+            res_c= res["coffee"]
+
+            res_cache["water"] = res_w - u_water
+            res_cache["milk"] = res_m - u_milk
+            res_cache["coffee"] = res_c - u_coffee
+        
+            #the cache's value is now the res value
+            res = res_cache
+
+            print("Thank you! Enjoy your coffee & have a nice day!")
             
     else:
-        break
+        print("\n"*20, logo)
+        print("Invalid choice ... Restarting transaction")
 
-print("\n"*20, logo)
-print("Invalid choice ... Restarting transaction")
+
              
 
 
